910b17f8a6dcec4aa08fb373a443ed52d22fc2e0
SiminaC <simaaa30@yahoo.com>
2018-01-29 23:37:05 +0200

    Created the MyAnno page
    Author:    SiminaC <simaaa30@yahoo.com>

--- src/components/MyAnno/MyAnno.tsx
+++ src/components/MyAnno/MyAnno.tsx
@@ -1,7 +1,81 @@
+import { Icon } from '@blueprintjs/core';
 import * as React from 'react';
+import './MyAnno.css';
 
 export class MyAnno extends React.Component {
   render() {
-    return <div>MyAnno page it's work!</div>;
+    const allAnno = JSON.parse(localStorage.getItem('anno'));
+
+    const annoCard = allAnno.map(annotation => {
+        let content;
+        switch (annotation.type) {
+          case 'image':
+            content = (
+              <div className='myAnno--details'>
+                <img className='myAnno--image' src={annotation.annotation.image}/>
+              </div>);
+            break;
+          case 'text':
+            content = (
+              <div className='myAnno--details'>
+                <div>
+                  <Icon iconName='pt-icon-label'/>
+                  {annotation.annotation.text}
+                </div>
+              </div>
+            );
+            break;
+          case 'person':
+            content = (
+              <div className='myAnno--details'>
+                <div>
+                  <Icon iconName='pt-icon-id-number'/>
+                  {annotation.annotation.type}
+                </div>
+                <div>
+                  <Icon iconName='pt-icon-person'/>
+                  {annotation.annotation.firstName}
+                </div>
+                <div>
+                  <Icon iconName='pt-icon-user'/>
+                  {annotation.annotation.lastName}
+                </div>
+              </div>
+            );
+            break;
+          case 'link':
+            content = (
+              <div className='myAnno--details'>
+                <div>
+                  <Icon iconName='pt-icon-link'/>
+                  <a href={annotation.annotation.linl}> aici </a>
+                </div>
+              </div>
+            );
+            break;
+          default:
+            content = (<div className='pt-elevation-3 pt-intent-danger' key={annotation.id}> unknown type of annotation </div>);
+        }
+        return (
+          <div className='pt-elevation-3 myAnno--card' key={annotation.id}>
+            <a href={annotation.url}><h3 className='pt-navbar myAnno--title'>{annotation.title}</h3></a>
+            <div className='myAnno--content'>
+              <div className='myAnno--info'>
+                <Icon iconName={'pt-icon-time'}/>
+                <span>{annotation.time}</span>
+                <div className='pt-tag'>{annotation.type}</div>
+              </div>
+              {content}
+            </div>
+          </div>
+        );
+      }
+    );
+
+    return (
+      <div className='myAnno--container'>
+        {annoCard}
+      </div>
+    );
   }
 }
